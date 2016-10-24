b9ec0ed0e29191fc7919dc5ee4e182681578ae64
kirito96 <kirito96@users.noreply.github.com>
2016-10-24 13:38:10 -0400

    Update and rename rainbow-vendor.mk to yaris-vendor.mk

---
+++ yaris-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alcatel/yaris/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alcatel/yaris/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alcatel/yaris/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alcatel/yaris/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alcatel/yaris/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/alcatel/yaris/gapps/system,system)
